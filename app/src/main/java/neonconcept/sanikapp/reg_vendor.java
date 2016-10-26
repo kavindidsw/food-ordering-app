2cbcc60d73236cb7806014bda43e48d5638557a4
Sahan Dissanayake <sadsahan@gmail.com>
2016-10-26 23:45:52 +0530

    send async request and save vendors in remote DB

--- app/src/main/java/neonconcept/sanikapp/reg_vendor.java
+++ app/src/main/java/neonconcept/sanikapp/reg_vendor.java
@@ -2,6 +2,7 @@ package neonconcept.sanikapp;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,11 +10,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 
 public class reg_vendor extends AppCompatActivity implements View.OnClickListener{
 
     Button saveNLogin, toLogin;
     EditText fnamex,lnamex,unamex,emailx,hnamex,pwdx,conpwdx;
+    private static final String REGISTER_URL = "http://192.168.0.101/minion/vendorreg.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +60,7 @@ public class reg_vendor extends AppCompatActivity implements View.OnClickListene
             String fname = fnamex.getText().toString();
             String lname = lnamex.getText().toString();
             String uname = unamex.getText().toString();
-            String email = unamex.getText().toString();
+            String email = emailx.getText().toString();
             String hname = hnamex.getText().toString();
             String pwd = pwdx.getText().toString();
             String confpwd = conpwdx.getText().toString();
@@ -112,6 +119,7 @@ public class reg_vendor extends AppCompatActivity implements View.OnClickListene
                     /*
                     Change intent, Redirect user to login page with username and password
                      */
+                    registerVendor(fname,lname,hname,uname,pwd,email);
                     Intent login = new Intent(this, neonconcept.sanikapp.login.class);
                     login.putExtra("username",uname);
                     login.putExtra("pawd",pwd);
@@ -131,5 +139,42 @@ public class reg_vendor extends AppCompatActivity implements View.OnClickListene
             login.putExtra("pawd","");
             startActivity(login);
         }
+    }
+
+    private void registerVendor(String fname, String lname, String hotel,String username, String password, String email) {
+        System.out.println("parsed email is = "+email);
+        String urlSuffix = "fname="+fname+"&lname="+lname+"&hotel="+hotel+"&username="+username+"&password="+password+"&email="+email;
+        class RegVendor extends AsyncTask<String, String[] , String>{
+
+            @Override
+            protected String doInBackground(String... strings) {
+
+                try {
+                    String valswithurl = strings[0];
+                    System.out.println("URL : "+valswithurl);
+                    URL url = new URL(valswithurl);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    BufferedReader df = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    System.out.println("read line : "+df.readLine());
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                //Toast.makeText(getApplicationContext(),"before excecute",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                Toast.makeText(getApplicationContext(),"Post excecute",Toast.LENGTH_LONG).show();
+            }
+        }
+        RegVendor regven = new RegVendor();
+        regven.execute(REGISTER_URL+"?"+urlSuffix);
     }
 }
