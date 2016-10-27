899dd6a52a020cfe7913bac57574884b2253797b
Sahan Dissanayake <sadsahan@gmail.com>
2016-10-27 17:05:39 +0530

    show hotel details in alert dialog

--- app/src/main/java/neonconcept/sanikapp/ListOption.java
+++ app/src/main/java/neonconcept/sanikapp/ListOption.java
@@ -2,11 +2,30 @@ package neonconcept.sanikapp;
 
 public class ListOption {
     private int hotel_id;
+    private int vendor_id;
     private String hotel_name;
+    private String reg_no;
+    private String address;
+    private String email;
+    private String phone;
+    private String phone_land;
+    private String description;
+    private String open_hrs;
+    private String postal_code;
 
-    public ListOption(int param_id, String param_hotel_name){
+    public ListOption(int param_id, int p_vendor, String param_hotel_name, String pram_reg_no, String param_addr, String p_email, String p_phone, String p_land, String p_description,String p_openhrs, String p_postalcode){
         this.hotel_id = param_id;
+        this.vendor_id = p_vendor;
         this.hotel_name = param_hotel_name;
+
+        this.reg_no = pram_reg_no;
+        this.address = param_addr;
+        this.email = p_email;
+        this.phone = p_phone;
+        this.phone_land = p_land;
+        this.description = p_description;
+        this.open_hrs = p_openhrs;
+        this.postal_code = p_postalcode;
     }
 
     @Override
@@ -21,5 +40,37 @@ public class ListOption {
 
     public String getHotel_name() {
         return hotel_name;
+    }
+
+    public String getReg_no() {
+        return reg_no;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getPhoneLand() {
+        return phone_land;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getOpen_hrs() {
+        return open_hrs;
+    }
+
+    public String getPostal_code() {
+        return postal_code;
     }
 }
--- app/src/main/java/neonconcept/sanikapp/hotel_list.java
+++ app/src/main/java/neonconcept/sanikapp/hotel_list.java
@@ -1,9 +1,14 @@
 package neonconcept.sanikapp;
 
+import android.content.Context;
 import android.os.AsyncTask;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,7 +22,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
 
-public class hotel_list extends AppCompatActivity {
+public class hotel_list extends AppCompatActivity implements AdapterView.OnItemLongClickListener{
 
     String FETCH_HOTELS = "http://192.168.0.101/minion/gethotels.php"; // Get hotels PHP
     TextView userTV;
@@ -44,6 +49,8 @@ public class hotel_list extends AppCompatActivity {
 
         // fetch registered hotels
         fetchHotels(vendor_id);
+
+        list.setOnItemLongClickListener(this);
     }
 
     private void fetchHotels(final String vendor_id) {
@@ -69,7 +76,19 @@ public class hotel_list extends AppCompatActivity {
 
                     JSONArray hotels = job.getJSONArray("hotels");
                     for (int i = 0; i < hotels.length(); i++) {
-                        list_data.add(new ListOption(Integer.parseInt(hotels.getJSONObject(i).getString("HotelId")), hotels.getJSONObject(i).getString("HotelName")));
+                        int vendor = Integer.parseInt(hotels.getJSONObject(i).getString("VendorId"));
+                        int hotel_id = Integer.parseInt(hotels.getJSONObject(i).getString("HotelId"));
+                        String hname = hotels.getJSONObject(i).getString("HotelName");
+                        String reg_no = hotels.getJSONObject(i).getString("RegistrationNo");
+                        String addr = hotels.getJSONObject(i).getString("Address");
+                        String email = hotels.getJSONObject(i).getString("Email");
+                        String phone = hotels.getJSONObject(i).getString("PhoneNumber");
+                        String pland = hotels.getJSONObject(i).getString("LandLine");
+                        String desc = hotels.getJSONObject(i).getString("Description");
+                        String openhrs = hotels.getJSONObject(i).getString("Opening Hours");
+                        String pcode = hotels.getJSONObject(i).getString("Postalcode");
+
+                        list_data.add(new ListOption(hotel_id,vendor,hname,reg_no,addr,email,phone,pland,desc,openhrs,pcode));
                     }
                 }catch (Exception e){
                     e.printStackTrace();
@@ -95,5 +114,51 @@ public class hotel_list extends AppCompatActivity {
 
         FetchHotels fhs = new FetchHotels();
         fhs.execute();
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        ListOption lop = (ListOption) adapterView.getItemAtPosition(i);
+        alert.setTitle(lop.getHotel_name());
+
+        Context context = this.getBaseContext();
+
+        LinearLayout layout = new LinearLayout(context);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.setPadding(50,0,0,50);
+
+        final TextView reg_no = new TextView(this);
+        reg_no.setText("Registration Number : "+lop.getReg_no());
+
+        final TextView addr = new TextView(this);
+        addr.setText("Address : "+lop.getAddress());
+
+        final TextView pcode = new TextView(this);
+        pcode.setText("Postal Code : "+lop.getPostal_code());
+
+        final TextView pno = new TextView(this);
+        pno.setText("Phone Number : "+lop.getPhone());
+
+        final TextView lpno = new TextView(this);
+        lpno.setText("Land Line : "+lop.getPhoneLand());
+
+        final TextView descr = new TextView(this);
+        descr.setText("Description : "+lop.getDescription());
+
+        final TextView ophrs = new TextView(this);
+        ophrs.setText("Open Hours : "+lop.getOpen_hrs());
+
+        layout.addView(reg_no);
+        layout.addView(addr);
+        layout.addView(pcode);
+        layout.addView(pno);
+        layout.addView(lpno);
+        layout.addView(descr);
+        layout.addView(ophrs);
+
+        alert.setView(layout);
+        alert.show();
+        return false;
     }
 }
